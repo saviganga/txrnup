5213f71a44c10df1547eb1fc681881830090a31d
saviganga <akatugbasaviour@gmail.com>
2024-05-27 04:24:46 +0100

    feat: app setup

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/saviganga/txrnup/initialisers"
+)
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Hello, Ganga! Welcome to Go programming language.")
+}
+
+func init() {
+	initialisers.LoadEnv()
+}
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	app := fiber.New()
+
+	app.Get("/", welcome)
+	fmt.Println(port)
+
+	app.Listen(":" + port)
+}
