2eab4677201e066dad6c0ad35b426e592ebc24fc
saviganga <akatugbasaviour@gmail.com>
2024-05-30 06:08:54 +0100

    feat: get users - order_by query not working...check it out

--- controllers/xusers/xusers.go
+++ controllers/xusers/xusers.go
@@ -1,8 +1,6 @@
 package xusers
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/saviganga/txrnup/initialisers"
 	"github.com/saviganga/txrnup/models/xuser"
@@ -12,7 +10,6 @@ func CreateUsers(c *fiber.Ctx) error {
 	// fmt.Println("Now i'm creating users")
 	db := initialisers.ConnectDb().Db
 	user := new(xuser.Xuser)
-	fmt.Println(len(user.Email))
 	err := c.BodyParser(user)
 	err = db.Create(&user).Error
 
@@ -22,4 +19,11 @@ func CreateUsers(c *fiber.Ctx) error {
 		})
 	}
 	return c.Status(200).JSON(user)
+}
+
+func GetUsers(c *fiber.Ctx) error {
+	db := initialisers.ConnectDb().Db
+	users := []xuser.Xuser{}
+	db.Model(&xuser.Xuser{}).Find(&users).Order("created_at desc")
+	return c.Status(200).JSON(users)
 }
--- handlers/xuser/xuser.go
+++ handlers/xuser/xuser.go
@@ -1,18 +1,15 @@
 package xuser
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/saviganga/txrnup/controllers/xusers"
 )
 
 func GetUsers(c *fiber.Ctx) error {
-	return c.SendString("Now i'm gonna try to fetch users")
+	return xusers.GetUsers(c)
+
 }
 
 func CreateUsers(c *fiber.Ctx) error {
-	fmt.Println("Now i'm gonna try to create users")
-
 	return xusers.CreateUsers(c)
 }
